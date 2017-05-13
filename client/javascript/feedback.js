f42a812fffd01eadec4e0ad29522ab032d305b02
Charles Goh <charlesgoh@59-36.priv23.nus.edu.sg>
2017-05-13 11:03:00 +0800

    [CLIENT] Added more template functions and created css styles for feedback template

--- client/javascript/feedback.js
+++ client/javascript/feedback.js
@@ -1,5 +1,16 @@
 import {Meteor} from 'meteor/meteor';
 
+
+
+Template.feedback.onCreated(function() {
+
+
+});
+
+Template.feedback.events({
+
+})
+
 Template.feedback.helpers({
     myTitle: function() {
         return "Feedback App";
